c8c90ab7af732d3d7432673ef2524dfc09de5005
JulioFCORI <127279133+JulioFCORI@users.noreply.github.com>
2026-03-15 00:38:04 +0000

    Update Mongo CRUD

---
+++ Práctica en línea/Mongo DB/CRUD Mongo/src/models/Task.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+const taskSchema = new mongoose.Schema({
+    description:{
+        type:String,
+        required: true
+    },
+    complete:{
+        type:Boolean,
+        default:false
+    }
+});
+
+const Task = mongoose.model('Task',taskSchema);
+export default Task;
