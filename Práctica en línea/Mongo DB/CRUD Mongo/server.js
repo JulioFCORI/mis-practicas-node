2d230f2685ad5fd6fc3e886a8a8be8eb0985d4d2
JulioFCORI <127279133+JulioFCORI@users.noreply.github.com>
2026-03-15 22:48:00 +0000

    Add jws to the project CRUD Mongo

--- Práctica en línea/Mongo DB/CRUD Mongo/server.js
+++ Práctica en línea/Mongo DB/CRUD Mongo/server.js
@@ -1,7 +1,28 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import connectDB from "./src/config/database.js";
 import TaskRoutes from './src/routes/TaskRoutes.js';
 const port = 3000;
+
+//User data
+const user = {
+    id:123,
+    name:'Ana',
+    role:'admin'
+};
+
+const token = jwt.sign(usuario, 'clave_secreta',{expiresIn:'1h'});
+console.log('Token jwt = ',token);
+
+const validateToken = (() => {
+    try{
+        const data = jwt.verify(token,'clave_secreta');
+        console.log('Token validate: ', data);
+    }catch(err){
+        console.log('Invalid token',err);
+
+    }
+});
 
 const app = express();
 
@@ -11,6 +32,7 @@ app.use(express.json());
 
 app.get('/',(req, res) => {
     res.send('Welcome to Tasjs API');
+    validateToken();
 });
 
 app.use('/api',TaskRoutes);
